8847d49e619aa328c3aaa14b0a72c1192afee474
isabelwang30 <103291789+isabelwang30@users.noreply.github.com>
2024-06-04 02:06:13 -0700

    sorted non_gene_ecDNA output

---
+++ non_gene_sort.py
@@ -0,0 +1,18 @@
+from non_gene_ecDNA import load_ecDNA
+
+# Load the filtered ecDNA rows
+ecDNA_file = 'non_gene_ecDNA_rows' # change relative path if needed
+ecDNAs = load_ecDNA(ecDNA_file)
+
+# sort by chromosome
+ecDNAs.sort(key=lambda a: a[0])
+
+# Output file for filtered intervals
+output_file = 'non_gene_ecDNA_sorted'
+
+# Open the output file
+with open(output_file, 'w') as outfile:
+    for ecDNA in ecDNAs:
+        # Convert ecDNA tuple to tab-delimited string
+        ecDNA = '\t'.join(map(str, ecDNA))
+        outfile.write(f"{ecDNA}\n")
